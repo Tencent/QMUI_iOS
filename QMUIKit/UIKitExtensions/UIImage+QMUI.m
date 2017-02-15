95bbd513388832b687bc1d597967349179280d22
从今以后 <949478479@qq.com>
2017-02-15 23:23:46 +0800

    修复 -[UIImage(QMUI) qmui_averageColor] RGB 值计算问题

--- QMUIKit/UIKitExtensions/UIImage+QMUI.m
+++ QMUIKit/UIKitExtensions/UIImage+QMUI.m
@@ -32,26 +32,24 @@ CGSizeFlatSpecificScale(CGSize size, float scale) {
 }
 
 - (UIColor *)qmui_averageColor {
-    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    unsigned char rgba[4];
-    CGContextRef context = CGBitmapContextCreate(rgba, 1, 1, 8, 4, colorSpace, kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
-    CGContextInspectContext(context);
-    CGContextDrawImage(context, CGRectMake(0, 0, 1, 1), self.CGImage);
-    CGColorSpaceRelease(colorSpace);
-    CGContextRelease(context);
-    if(rgba[3] > 0) {
-        CGFloat alpha = ((CGFloat)rgba[3]) / 255.0;
-        CGFloat multiplier = alpha / 255.0;
-        return [UIColor colorWithRed:((CGFloat)rgba[0]) * multiplier
-                               green:((CGFloat)rgba[1]) * multiplier
-                                blue:((CGFloat)rgba[2]) * multiplier
-                               alpha:alpha];
-    } else {
-        return [UIColor colorWithRed:((CGFloat)rgba[0])/255.0
-                               green:((CGFloat)rgba[1])/255.0
-                                blue:((CGFloat)rgba[2])/255.0
-                               alpha:((CGFloat)rgba[3])/255.0];
-    }
+	unsigned char rgba[4] = {};
+	CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+	CGContextRef context = CGBitmapContextCreate(rgba, 1, 1, 8, 4, colorSpace, kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+	CGContextInspectContext(context);
+	CGContextDrawImage(context, CGRectMake(0, 0, 1, 1), self.CGImage);
+	CGColorSpaceRelease(colorSpace);
+	CGContextRelease(context);
+	if(rgba[3] > 0) {
+		return [UIColor colorWithRed:((CGFloat)rgba[0] / rgba[3])
+							   green:((CGFloat)rgba[1] / rgba[3])
+								blue:((CGFloat)rgba[2] / rgba[3])
+							   alpha:((CGFloat)rgba[3] / 255.0)];
+	} else {
+		return [UIColor colorWithRed:((CGFloat)rgba[0])/255.0
+							   green:((CGFloat)rgba[1])/255.0
+								blue:((CGFloat)rgba[2])/255.0
+							   alpha:((CGFloat)rgba[3])/255.0];
+	}
 }
 
 - (UIImage *)qmui_grayImage {
