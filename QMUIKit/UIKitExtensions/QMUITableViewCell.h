4ac86d82888ab0e2d11fc6386b5b041ab261e053
MoLice <sf.molice@gmail.com>
2017-03-06 14:51:41 +0800

    修改 QMUITableViewCell cellPosition 的注释

--- QMUIKit/UIKitExtensions/QMUITableViewCell.h
+++ QMUIKit/UIKitExtensions/QMUITableViewCell.h
@@ -41,7 +41,12 @@
 /// 保存对tableView的弱引用，在布局时可能会使用到tableView的一些属性例如separatorColor等。只有使用下面两个 initForTableView: 的接口初始化时这个属性才有值，否则就只能自己初始化后赋值
 @property(nonatomic, weak) QMUITableView *parentTableView;
 
-/// cell处于section中的位置，只有在cellForRowAtIndexPath里面调用了updateCellAppearanceWithIndexPath:才会去设置这个position
+/**
+ *  cell 处于 section 中的位置，要求：
+ *  1. cell 使用 initForTableViewXxx 方法初始化，或者初始化完后为 parentTableView 属性赋值。
+ *  2. 在 cellForRow 里调用 [cell updateCellAppearanceWithIndexPath:] 方法。
+ *  3. 之后即可通过 cellPosition 获取到正确的位置。
+ */
 @property(nonatomic, assign, readonly) QMUITableViewCellPosition cellPosition;
 
 /**
