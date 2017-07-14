a83f0e4876fd17e1442b662aeb3134113178b371
MoLice <sf.molice@gmail.com>
2017-07-14 19:39:06 +0800

    UILabel(QMUI) 增加 qmui_lineHeight 用于方便地设置行高

--- QMUIKit/UIKitExtensions/UILabel+QMUI.h
+++ QMUIKit/UIKitExtensions/UILabel+QMUI.h
@@ -18,6 +18,8 @@
  * @note 即使先调用 setText/attributedText ，然后再设置此属性，此属性仍然会生效
  * @note 如果此属性包含了 NSKernAttributeName ，则最后一个字的 kern 效果会自动被移除，否则容易导致文字在视觉上不居中
  *
+ * @note 当你设置了此属性后，每次你调用 setText: 时，其实都会被自动转而调用 setAttributedText:
+ *
  * 现在你有三种方法控制 label 的样式：
  * 1. 本身的样式属性（如 textColor, font 等）
  * 2. qmui_textAttributes
@@ -27,6 +29,18 @@
  *
  */
 @property(nonatomic, copy) NSDictionary<NSString *, id> *qmui_textAttributes;
+
+/** 
+ *  设置当前整段文字的行高
+ *  @note 如果同时通过 qmui_textAttributes 或 attributedText 给整段文字设置了行高，则此方法将不再生效。换句话说，此方法设置的行高将永远不会覆盖 qmui_textAttributes 或 attributedText 设置的行高。
+ *  @note 比如对于字符串"abc"，你通过 attributedText 设置 {0, 1} 这个 range 范围内的行高为 10，又通过 setQmui_lineHeight: 设置了整体行高为 20，则最终 {0, 1} 内的行高将为 10，而 {1, 2} 内的行高将为全局行高 20
+ *  @note 比如对于字符串"abc"，你先通过 setQmui_lineHeight: 设置整体行高为 10，又通过 attributedText/qmui_textAttributes 设置整体行高为 20，无论这两个设置的代码的先后顺序如何，最终行高都将为 20
+ *
+ *  @note 当你设置了此属性后，每次你调用 setText: 时，其实都会被自动转而调用 setAttributedText:
+ *
+ */
+
+- (void)setQmui_lineHeight:(CGFloat)qmui_lineHeight;
 
 /**
  * 将目标UILabel的样式属性设置到当前UILabel上
