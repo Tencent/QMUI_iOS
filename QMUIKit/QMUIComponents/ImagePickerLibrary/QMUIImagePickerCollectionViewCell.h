221e2aa889d2ee6d73ba900846f1951901187a0a
MoLice <sf.molice@gmail.com>
2018-02-07 17:04:57 +0800

    相册控件选图时优化对 iCloud 加载的方式，否则以前可能无法发送 iCloud 图片

--- QMUIKit/QMUIComponents/ImagePickerLibrary/QMUIImagePickerCollectionViewCell.h
+++ QMUIKit/QMUIComponents/ImagePickerLibrary/QMUIImagePickerCollectionViewCell.h
@@ -11,8 +11,6 @@
 #import "QMUIAsset.h"
 #import "QMUIButton.h"
 
-@class QMUIPieProgressView;
-
 // checkbox 的 margin 默认值
 extern const UIEdgeInsets QMUIImagePickerCollectionViewCellDefaultCheckboxButtonMargins;
 
@@ -29,12 +27,6 @@ extern const UIEdgeInsets QMUIImagePickerCollectionViewCellDefaultCheckboxButton
 
 /// checkbox 的 margin，定位从每个 cell（即每张图片）的最右边开始计算
 @property(nonatomic, assign) UIEdgeInsets checkboxButtonMargins UI_APPEARANCE_SELECTOR;
-
-/// progressView tintColor
-@property(nonatomic, strong) UIColor *progressViewTintColor UI_APPEARANCE_SELECTOR;
-
-/// downloadRetryButton 的 icon
-@property(nonatomic, strong) UIImage *downloadRetryImage UI_APPEARANCE_SELECTOR;
 
 /// videoMarkImageView 的 icon
 @property(nonatomic, strong) UIImage *videoMarkImage UI_APPEARANCE_SELECTOR;
@@ -53,13 +45,11 @@ extern const UIEdgeInsets QMUIImagePickerCollectionViewCellDefaultCheckboxButton
 
 @property(nonatomic, strong, readonly) UIImageView *contentImageView;
 @property(nonatomic, strong, readonly) QMUIButton *checkboxButton;
-@property(nonatomic, strong, readonly) QMUIPieProgressView *progressView;
-@property(nonatomic, strong, readonly) QMUIButton *downloadRetryButton;
 @property(nonatomic, strong, readonly) UIImageView *videoMarkImageView;
 @property(nonatomic, strong, readonly) UILabel *videoDurationLabel;
 @property(nonatomic, strong, readonly) CAGradientLayer *videoBottomShadowLayer;
 
-@property(nonatomic, assign, getter=isEditing) BOOL editing;
+@property(nonatomic, assign, getter=isSelectable) BOOL selectable;
 @property(nonatomic, assign, getter=isChecked) BOOL checked;
 @property(nonatomic, assign) QMUIAssetDownloadStatus downloadStatus; // Cell 中对应资源的下载状态，这个值的变动会相应地调整 UI 表现
 
