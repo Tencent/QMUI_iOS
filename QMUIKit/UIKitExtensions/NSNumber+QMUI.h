6f13181650fbf71b1fca0167dd341421b92f5443
MoLice <sf.molice@gmail.com>
2018-02-07 17:03:22 +0800

    增加 NSNumber (QMUI) 用于输出 CGFloat 类型的 value

---
+++ QMUIKit/UIKitExtensions/NSNumber+QMUI.h
@@ -0,0 +1,15 @@
+//
+//  NSNumber+QMUI.h
+//  QMUIKit
+//
+//  Created by MoLice on 2018/1/16.
+//  Copyright © 2018年 QMUI Team. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface NSNumber (QMUI)
+
+@property(nonatomic, assign, readonly) CGFloat qmui_CGFloatValue;
+@end
---
+++ QMUIKit/UIKitExtensions/NSNumber+QMUI.m
@@ -0,0 +1,21 @@
+//
+//  NSNumber+QMUI.m
+//  QMUIKit
+//
+//  Created by MoLice on 2018/1/16.
+//  Copyright © 2018年 QMUI Team. All rights reserved.
+//
+
+#import "NSNumber+QMUI.h"
+
+@implementation NSNumber (QMUI)
+
+- (CGFloat)qmui_CGFloatValue {
+#if defined(__LP64__) && __LP64__
+    return self.doubleValue;
+#else
+    return self.floatValue;
+#endif
+}
+
+@end
