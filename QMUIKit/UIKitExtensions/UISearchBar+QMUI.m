b8e01f497d50a6d8243b820204af8fd5f1543fbd
MoLice <sf.molice@gmail.com>
2018-02-07 17:01:46 +0800

    补上某些 property 的 nullable 标记

--- QMUIKit/UIKitExtensions/UISearchBar+QMUI.m
+++ QMUIKit/UIKitExtensions/UISearchBar+QMUI.m
@@ -10,6 +10,7 @@
 #import "QMUICore.h"
 #import "UIImage+QMUI.h"
 #import "UIView+QMUI.h"
+#import "QMUILog.h"
 
 @implementation UISearchBar (QMUI)
 
@@ -284,7 +285,7 @@ static char kAssociatedObjectKey_textFieldMargins;
     UIImage *searchIconImage = SearchBarSearchIconImage;
     if (searchIconImage) {
         if (!CGSizeEqualToSize(searchIconImage.size, CGSizeMake(13, 13))) {
-            QMUILog(@"搜索框放大镜图片（SearchBarSearchIconImage）的大小最好为 (13, 13)，否则会失真，目前的大小为 %@", NSStringFromCGSize(searchIconImage.size));
+            QMUILog(@"UISearchBar (QMUI)", @"搜索框放大镜图片（SearchBarSearchIconImage）的大小最好为 (13, 13)，否则会失真，目前的大小为 %@", NSStringFromCGSize(searchIconImage.size));
         }
         [self setImage:searchIconImage forSearchBarIcon:UISearchBarIconSearch state:UIControlStateNormal];
     }
